d9ec055db90ffac41b48123c3c9b355d124f3250
llanos <d47001835@alumnos.itr.edu.ar>
2024-08-05 16:50:27 -0300

    ejercicio terminado

--- src/todolist.jsx
+++ src/todolist.jsx
@@ -2,10 +2,22 @@ import react, { useState } from "react";
 import "./App.css";
 function TodoList() {
   const [tasks, setTasks] = useState([]);
-  const [neWTasks, setNewTasks] = useState("");
+  const [neWTasks, setNewTasks] = useState({
+    tarea:"",
+    categoria:""
+  });
 
   const handleChange = event => {
-    setNewTasks(event.target.value);
+    setNewTasks({
+      tarea:event.target.value,
+      categoria:neWTasks.categoria
+    });
+  };
+  const handleCategoria = event => {
+    setNewTasks({
+      tarea:neWTasks.tarea,
+      categoria:event.target.value
+    });
   };
   const addTask = event => {
     event.preventDefault();
@@ -20,12 +32,19 @@ function TodoList() {
       <form onSubmit={addTask}>
         <div>
           <h1>Tareas</h1>
-          <input type="text" value={neWTasks} onChange={handleChange} />
+          <input type="text" value={neWTasks.tarea} onChange={handleChange} />
+          <select onChange={handleCategoria}>
+            <option value=""></option>
+            <option value="Casa">casa</option>
+            <option value="Escuela">escuela</option>
+            <option value="Compras">compras</option>
+            <option value="Ocio">ocio</option>
+          </select>
         </div>
         <button onClick={addTask}>enviar formulario</button>
       </form>
       {tasks.map((task, index) => (
-        <li key={index}>{task} <button onClick={() => removeTask(task)}>Eliminar</button></li>
+        <li key={index}>Tarea: {task.tarea} categoria: {task.categoria} <button onClick={() => removeTask(task)}>Eliminar</button></li>
       ))}
     </div>
   );
